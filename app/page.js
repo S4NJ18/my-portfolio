64ef05d30dd32e827021a01c4e1bfa8a1d73f313
Sanjib <49269917+S4NJ18@users.noreply.github.com>
2025-07-25 11:10:04 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import { Metadata } from 'next';
 import SkillsCard from "@/component/SkillCard";
 import Image from "next/image";
 import Link from "next/link";
