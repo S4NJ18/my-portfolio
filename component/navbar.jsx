798d126c0a60e3f8c8eeacaa60dc5f470b0c218c
Sanjib <49269917+S4NJ18@users.noreply.github.com>
2025-07-29 15:13:24 +0530

    Update navbar.jsx

--- component/navbar.jsx
+++ component/navbar.jsx
@@ -80,7 +80,7 @@ const NavbarComponent = () => {
                 className="rounded px-6 py-3 flex gap-3 justify-center items-center bg-[#a53dff] w-full md:w-auto">
                 <p className="text-base leading-6 text-white font-semibold">
                   <Link href="https://drive.google.com/file/d/1-0h-eshMycQJzn4GWVBt63U2J7ZeWKUT/view?usp=sharing">
-                    My Resume
+                    See My Resume
                   </Link>
                 </p>
               </button>
